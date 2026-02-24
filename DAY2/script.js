9152086b937dcc31ff3b7ebdea2fb80bfdcbd1e7
Siddhant1020 <tilaksiddhant@gmail.com>
2026-02-24 17:56:33 +0530

    practice

---
+++ DAY2/script.js
@@ -0,0 +1,13 @@
+console.log("Hello world !!!!")
+
+console.log(1234)
+
+let sidd = "sidd"
+sidd = "siddhant"
+console.log(sidd)
+
+console.log("${sidd}")
+
+let names = "Siddhant";
+let age = 21 
+console.log(`My name is ${names} and My age is ${age}`)
